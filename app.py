90a47681cbc67574bc1d057037a60303dc6cff33
Daniel Fimaini <daniel.fimiani@mercadolibre.com>
2024-11-01 20:57:04 -0300

    remove comments

--- app.py
+++ app.py
@@ -1,17 +1,3 @@
-# import matplotlib.pyplot as plt
-# import numpy as np
-
-# t = np.arange(0.0, 2.0, 0.01)
-# s1 = np.sin(2*np.pi*t)
-
-# plt.figure(1)
-# plt.subplot(211)
-# plt.plot(t, s1)
-# # plt.subplot(212)
-# # plt.plot(t, 2*s1)
-
-# plt.show()
-
 import numpy as np
 import matplotlib.pyplot as plt
 
